2a12f9ea798212e1f2a3202b2472726a31c973c1
MAIN <dimoamoan@gmail.com>
2026-04-27 21:33:11 +0400

    fixed email field

--- forms/login_form.py
+++ forms/login_form.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 
-from wtforms import EmailField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = EmailField('User name:', validators=[DataRequired()])
+    username = StringField('User name:', validators=[DataRequired()])
     password = PasswordField('Пароль:', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня:')
     submit = SubmitField('Войти')
